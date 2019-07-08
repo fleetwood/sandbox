8254802138884ec7cdc336f428e7a18352974b2d
Fleetwood <wizening@gmail.com>
2019-07-08 09:51:48 -0500

    Simple template

---
+++ index.js
@@ -0,0 +1,7 @@
+const server = require('./server');
+
+const array = [1,2,3,4];
+
+console.log(array.map(a=> {
+    return a*a;
+}));
---
+++ sandbox.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    const log = (str) => {
+        const main = $('#main');
+        main.append(`${str}<br />`);
+    }
+    ///////////////////////////////
+
+    
+});
